a59abb1ee2cfee33f3eb811ce98daf9490663821
irfanhsajid <irfanhsajid@gmail.com>
2022-01-22 19:06:32 +0600

    first commit

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            Footer section here
+            <Button variant='contained'>Click Here</Button>
+        </div>
+    );
+};
+
+export default Footer;
